7910dec91ef9c1b86eee1b0dca96c44187696a75
Dylan Roubos <dylanroubos@gmail.com>
2020-04-23 10:20:33 +0200

    Initialising basic Maven project structure

---
+++ NerdyGadgetsApp/src/main/java/site/nerdy-gadgets/App.java
@@ -0,0 +1,13 @@
+package site.nerdy-gadgets;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
