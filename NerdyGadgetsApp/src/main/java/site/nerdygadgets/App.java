04420355990e868bc0c21ab3e4df3b4cf87b2e9b
Dylan Roubos <dylanroubos@gmail.com>
2020-04-29 22:03:20 +0200

    Fixed the SSHManager for development of the component display page

--- NerdyGadgetsApp/src/main/java/site/nerdygadgets/App.java
+++ NerdyGadgetsApp/src/main/java/site/nerdygadgets/App.java
@@ -12,9 +12,17 @@ public class App {
 
     public static void main(String[] args) {
 
-//        SSHManager ssh = new SSHManager();
+        SSHManager ssh = new SSHManager("student", "wd9AdEuN", "172.16.0.158");
 
-        System.out.println("joe simpels joe!");
+        String cpu = ssh.runCommand("top -bn2 | grep \"Cpu(s)\" | tail -n1 | sed \"s/.*, *\\([0-9.]*\\)%* id.*/\\1/\" | awk '{print 100 - $1}'");
+        String memory = ssh.runCommand("free --mega | grep 'Mem:' | awk '{print $3 \"M / \" $2 \"M\"}'");
+        String diskSpace = ssh.runCommand("df -h --total | grep 'total' | awk '{print $3 \" / \" $2}'");
+        String upTime = ssh.runCommand("uptime -p");
+
+        System.out.print("cpu: " + cpu);
+        System.out.print("memory: " + memory);
+        System.out.print("Disk space: " + diskSpace);
+        System.out.print("Uptime: " + upTime);
 
 
         JFrame.setDefaultLookAndFeelDecorated(true);
--- NerdyGadgetsApp/src/main/java/site/nerdygadgets/sandbox/SSHManager.java
+++ NerdyGadgetsApp/src/main/java/site/nerdygadgets/sandbox/SSHManager.java
@@ -7,49 +7,67 @@ import java.io.InputStream;
 
 //Simple SSH POC class to demonstrate usage of JSch
 public class SSHManager {
-    String user = "student";
-    String password = "KHxd4gu7";
-    String host = "172.16.0.190";
-    int port = 22;
-    String command1 = "ls -ltr";
+    private String user;
+    private String password;
+    private String host;
+    private int port = 22;
+    private Session session;
 
-    public SSHManager() {
+    public SSHManager(String user, String password, String host) {
+        this.user = user;
+        this.password = password;
+        this.host = host;
+
+        startSession();
+    }
+
+    public void startSession() {
         try {
             JSch jsch = new JSch();
             Session session = jsch.getSession(user, host, port);
             session.setPassword(password);
             session.setConfig("StrictHostKeyChecking", "no");
-            System.out.println("Establishing Connection...");
             session.connect();
-            System.out.println("Connection established.");
+            this.session = session;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    public String runCommand(String command) {
+
+        try {
             Channel channel = session.openChannel("exec");
-            ((ChannelExec) channel).setCommand(command1);
+            ((ChannelExec) channel).setCommand(command);
             channel.setInputStream(null);
             ((ChannelExec) channel).setErrStream(System.err);
-            InputStream in = channel.getInputStream();
+            InputStream outputCommand = channel.getInputStream();
             channel.connect();
+
+            //Put results in a String
             byte[] tmp = new byte[1024];
+            String result = "";
             while (true) {
-                while (in.available() > 0) {
-                    int i = in.read(tmp, 0, 1024);
+                while (outputCommand.available() > 0) {
+                    int i = outputCommand.read(tmp, 0, 1024);
                     if (i < 0) break;
-                    System.out.print(new String(tmp, 0, i));
+                    result += (new String(tmp, 0, i));
                 }
                 if (channel.isClosed()) {
                     System.out.println("exit-status: " + channel.getExitStatus());
                     break;
                 }
-                try {
-                    Thread.sleep(1000);
-                } catch (Exception ee) {
-                }
             }
             channel.disconnect();
-            session.disconnect();
-            System.out.println("DONE");
-        } catch (JSchException | IOException e) {
-            e.printStackTrace();
+            return result;
+
+        } catch (Exception e) {
+            System.out.println("Something went wrong with creating a channel or running the command");
+            return null;
         }
+    }
+
+    public void endSession(Session session) {
+        session.disconnect();
     }
 }
