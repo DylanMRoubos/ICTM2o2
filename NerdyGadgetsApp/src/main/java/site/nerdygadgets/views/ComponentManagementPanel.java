04a515a7690110e3c187e1f98383b11a5b607e62
Jordy Wielaard <jordy.wielaard@windesheim.nl>
2020-04-30 11:38:36 +0200

    Knoppen en textfields toegevoegd voor het aanmaken van componenten

--- NerdyGadgetsApp/src/main/java/site/nerdygadgets/views/ComponentManagementPanel.java
+++ NerdyGadgetsApp/src/main/java/site/nerdygadgets/views/ComponentManagementPanel.java
@@ -11,11 +11,16 @@ public class ComponentManagementPanel extends JPanel {
     private JLabel jlComponentToevoegen;
     private JPanel jpComponentToevegoen;
     private JPanel jpComponentAanmaken;
+
     private JLabel jlNaam;
     private JTextField jtNaam;
     private JLabel jlPrijs;
     private JTextField jtPrijs;
     private JLabel jlBeschikbaarheid;
+    private JTextField jtBeschikbaarheid;
+    String[] componenten = {"Database", "Webserver", "PFSense"};
+    private JComboBox jcComponenten;
+    private JButton jbVoegToe;
 
 
 
@@ -39,7 +44,7 @@ public class ComponentManagementPanel extends JPanel {
         jpHuidigeComponenten = new JPanel();
         add(jpHuidigeComponenten);
 //        jpHuidigeComponenten.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        jpHuidigeComponenten.add(jlHuidigeComponenten, BorderLayout.CENTER);
+        jpHuidigeComponenten.add(jlHuidigeComponenten);
 
         jpComponentToevegoen = new JPanel();
         add(jpComponentToevegoen);
@@ -48,5 +53,44 @@ public class ComponentManagementPanel extends JPanel {
         jpComponentToevegoen.add(jlComponentToevoegen);
         jpComponentToevegoen.add(jpComponentAanmaken);
 
+
+        jlNaam = new JLabel("Naam: ");
+        jlPrijs = new JLabel("Prijs: ");
+        jlBeschikbaarheid = new JLabel("Beschikbaarheid:");
+        jtNaam = new JTextField(5);
+        jtPrijs = new JTextField(5);
+        jtBeschikbaarheid = new JTextField(5);
+        jcComponenten = new JComboBox(componenten);
+        jbVoegToe = new JButton("Voeg Toe");
+
+        jpComponentAanmaken.setLayout(new FlowLayout());
+        jpComponentAanmaken.add(jlNaam);
+        jpComponentAanmaken.add(jtNaam);
+        jpComponentAanmaken.add(jlPrijs);
+        jpComponentAanmaken.add(jtPrijs);
+        jpComponentAanmaken.add(jlBeschikbaarheid);
+        jpComponentAanmaken.add(jtBeschikbaarheid);
+        jpComponentAanmaken.add(jcComponenten);
+        jpComponentAanmaken.add(jbVoegToe);
+    }
+
+    public JTextField getJtNaam(){
+        return jtNaam;
+    }
+
+    public JTextField getJtPrijs() {
+        return jtPrijs;
+    }
+
+    public JTextField getJtBeschikbaarheid() {
+        return jtBeschikbaarheid;
+    }
+
+    public JComboBox getJcComponenten() {
+        return jcComponenten;
+    }
+
+    public JButton getJbVoegToe() {
+        return jbVoegToe;
     }
 }
