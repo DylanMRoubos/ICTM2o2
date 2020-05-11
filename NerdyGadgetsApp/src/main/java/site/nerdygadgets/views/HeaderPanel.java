608939a0296bd69f05259c6ea8ccf99db6013d04
Mike Thomas <mike.thomas2402@gmail.com>
2020-05-11 11:42:00 +0200

    Change HeaderPanel
    Add Border on bottom of HeaderPanel, set font and sizing on header text.

--- NerdyGadgetsApp/src/main/java/site/nerdygadgets/views/HeaderPanel.java
+++ NerdyGadgetsApp/src/main/java/site/nerdygadgets/views/HeaderPanel.java
@@ -1,13 +1,17 @@
 package site.nerdygadgets.views;
 
 import javax.swing.*;
+import javax.swing.border.MatteBorder;
+import java.awt.*;
 
 public class HeaderPanel extends JPanel {
     private JLabel headerText;
 
     public HeaderPanel() {
         headerText = new JLabel("NerdyGadgets");
+        headerText.setFont(new Font("Segoe UI", Font.BOLD,  18));
         add(headerText);
+        setBorder(new MatteBorder(0, 0, 1, 0, Color.BLACK));
     }
 
     public JLabel getHeaderText() {
