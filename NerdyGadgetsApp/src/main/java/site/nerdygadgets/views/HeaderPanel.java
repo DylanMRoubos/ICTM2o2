ff333ed857ed352fa03619ce37bbae6e4c93652e
Mike Thomas <mike.thomas2402@gmail.com>
2020-05-11 14:18:09 +0200

    Header text bigger font

--- NerdyGadgetsApp/src/main/java/site/nerdygadgets/views/HeaderPanel.java
+++ NerdyGadgetsApp/src/main/java/site/nerdygadgets/views/HeaderPanel.java
@@ -9,7 +9,7 @@ public class HeaderPanel extends JPanel {
 
     public HeaderPanel() {
         headerText = new JLabel("NerdyGadgets");
-        headerText.setFont(new Font("Segoe UI", Font.BOLD,  18));
+        headerText.setFont(new Font("Segoe UI", Font.BOLD,  24));
         add(headerText);
         setBorder(new MatteBorder(0, 0, 1, 0, Color.BLACK));
     }
